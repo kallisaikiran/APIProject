8a3555cfba491a476d3e0bdb7d32980e0c6b7ef9
Harish babu alle <harishbabualle@gmail.com>
2021-05-23 20:41:26 -0400

    Movies Api added to the project and implemented the basic functionalities

---
+++ app/src/main/java/com/example/finalproj/Credentials.java
@@ -0,0 +1,8 @@
+package com.example.finalproj;
+
+public class Credentials {
+
+    public static final  String Base_Url="https://api.themoviedb.org/";
+    public static final String Api_Key="0b5014bca2d395d4d7fda9f951987cae";
+
+}
---
+++ app/src/main/java/com/example/finalproj/ServiceGenerator.java
@@ -0,0 +1,18 @@
+package com.example.finalproj;
+
+import com.apiprojects.tmdbapp.RetrofitModel.MovieApi;
+
+import androidx.annotation.NonNull;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ServiceGenerator {
+
+    private static Retrofit.Builder builder = new Retrofit.Builder().baseUrl(Credentials.Base_Url).addConverterFactory(GsonConverterFactory.create());
+    private static Retrofit retrofit=builder.build();
+
+    private  static MovieApi movieApi=retrofit.create(MovieApi.class);
+    public static MovieApi getMovieApi(){
+        return movieApi;
+    }
+}
