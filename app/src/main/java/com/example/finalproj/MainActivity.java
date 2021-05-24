9b3c96cdf3eb341e2749d43e97219b9ed16d0bd1
kallisaikiran <kalli.saikiran@outlook.com>
2021-05-24 17:07:15 -0400

    updated main activity

--- app/src/main/java/com/example/finalproj/MainActivity.java
+++ app/src/main/java/com/example/finalproj/MainActivity.java
@@ -1,14 +1,80 @@
 package com.example.finalproj;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.apiprojects.tmdbapp.Fragments.HomeFragment;
+import com.apiprojects.tmdbapp.Fragments.LatestMoviesFragment;
+import com.apiprojects.tmdbapp.Fragments.SearchFragment;
+import com.apiprojects.tmdbapp.Fragments.UpcomingFragment;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
+
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    HomeFragment.homeFragment()).commit();
+            navigationView.setCheckedItem(R.id.home);
+        }
     }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        HomeFragment.homeFragment() ).commit();
+                break;
+            case R.id.hotels:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        LatestMoviesFragment.latestMoviesFragment()  ).commit();
+                break;
+            case R.id.profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        SearchFragment.searchFragment()).commit();
+                break;
+            case R.id.booking:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                       UpcomingFragment.upcomingFragment()).commit();
+            case R.id.logout:
+                Toast.makeText(this, "Send", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
 }
